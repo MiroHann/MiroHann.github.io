5e8d9bd63d3f79c030601a5ef7c8e4afd0a9c53d
MiroHann <122731952+MiroHann@users.noreply.github.com>
2023-10-09 21:24:11 +0300

    Updated functionality

--- ContactJS.js
+++ ContactJS.js
@@ -1,31 +1,85 @@
 let formdata = document.querySelector('form');
-formdata.addEventListener('submit', async event => {
-    event.preventDefault();
-    const Data = new FormData(formdata);
-    const FormattedFormData = new URLSearchParams(Data);
-    
-  fetch("http://127.0.0.1:3000/comment/", {
-  method: 'POST',
-  body:  FormattedFormData,
-  mode: 'no-cors',
-  })
+var firstname = document.getElementById("firstname");
+var lastname = document.getElementById("lastname");
+var email = document.getElementById("emailadress");
+var comment = document.getElementById("subject");
+var warningtext = document.getElementById("warning")
+class connection{
+  constructor() {
   }
-  );
-function Submit(){
-  const Data = new FormData(formdata);
-  Name = Data.get("fname");
-  if(Name == ''){
-    alert('Thank you for your submission!');
-  } else {
-  alert('Thank you ' + Name + '!');
+  post(Data){
+    fetch("http://127.0.0.1:3000/comment/", {
+      method: 'POST',
+      body: Data,
+      mode: 'no-cors',
+    }).catch(function () {
+      console.log("connection error");
+    });
   }
 }
-function Images(){
+class verification extends connection{
+  constructor() {
+    super();
+  }
+  connectionTest(FormData) {
+    fetch("http://127.0.0.1:3000/comment/", {
+    })
+      .then(response => {
+        if (!response.ok) {
+          console.log("Network Error");
+        } else {
+          console.log("The connection was made"); 
+          super.post(FormData);
+        }
+      }).catch(function () {
+        console.log("connection error");
+        warningtext.innerHTML = "Sorry, There was a connection error, please try again later!";
+        warningtext.style.visibility = 'visible';
+        window.setTimeout(function(){
+          warningtext.style.visibility = 'hidden';
+        }, 20000);
+      });
+  }
+  formverification(Form) {
+    if (Form.get("fname")  != "" && Form.get("email") != "" && Form.get("comment") != "") {
+      this.connectionTest(new URLSearchParams(Form));
+    } else {
+      warningtext.innerHTML = "Please check the imput fields";
+      warningtext.style.visibility = 'visible';
+      window.setTimeout(function(){
+        warningtext.style.visibility = 'hidden';
+      }, 20000);
+      if(Form.get("fname") === ""){
+        firstname.style.backgroundColor = 'rgba(' + 255 + ',' + 99 + ',' + 71 + ',' + 0.5 + ')'
+      } 
+      if (Form.get("email") === ""){
+        email.style.backgroundColor = 'rgba(' + 255 + ',' + 99 + ',' + 71 + ',' + 0.5 + ')'
+      } 
+      if (Form.get("comment") === "") {
+        subject.style.backgroundColor = 'rgba(' + 255 + ',' + 99 + ',' + 71 + ',' + 0.5 + ')'
+      }
+      window.setTimeout(function(){
+       firstname.style.backgroundColor = 'rgba(' + 255 + ',' + 255 + ',' + 255 + ',' + 0.8 + ')';
+       email.style.backgroundColor = 'rgba(' + 255 + ',' + 255 + ',' + 255 + ',' + 0.8 + ')';
+       subject.style.backgroundColor = 'rgba(' + 255 + ',' + 255 + ',' + 255 + ',' + 0.8 + ')';
+      }, 8000);
+    }
+  }
+}
+formdata.addEventListener('submit', async event => {
+  event.preventDefault();
+  const Data = new FormData(formdata);
+  VerifiTest.formverification(Data);
+}
+);
+function Images() {
   fetch("https://dog.ceo/api/breeds/image/random")
-  .then(res => res.json())
-  .then(result =>{
-    console.log(result)
-    dog.src = result.message
-  })
-
+    .then(res => res.json())
+    .then(result => {
+      console.log(result)
+      dog.src = result.message
+    })
+}
+function init() {
+  VerifiTest = new verification();
 }
